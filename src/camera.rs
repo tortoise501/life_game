1b1398a90b29d4dd433eecaa7f6a638ddd3bf04c
danylo <danylo@desktop-1ue2cha.home>
2024-06-19 21:10:41 +0200

    camera movement and zoom (without delta time)

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,8 @@
-use bevy::prelude::*;
+use bevy::{
+    ecs::query::{self, QuerySingleError},
+    input::mouse::{MouseMotion, MouseWheel},
+    prelude::*,
+};
 
 const CAMERA_DISTANCE: f32 = 24.0;
 
@@ -6,7 +10,9 @@ pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, camera_zoom)
+            .add_systems(Update, camera_movement);
     }
 }
 
@@ -15,4 +21,50 @@ fn spawn_camera(mut commands: Commands) {
         transform: Transform::from_xyz(0.0, 2.0, 0.0),
         ..default()
     });
+}
+
+fn camera_zoom(
+    mut evr_scroll: EventReader<MouseWheel>,
+    mut query: Query<&mut OrthographicProjection>,
+) {
+    use bevy::input::mouse::MouseScrollUnit;
+    for ev in evr_scroll.read() {
+        for mut projection in &mut query {
+            match ev.unit {
+                MouseScrollUnit::Line => {
+                    println!(
+                        "Scroll (line units): vertical: {}, horizontal: {}",
+                        ev.y, ev.x
+                    );
+                    projection.scale -= 0.1 * ev.y; //? add delta seconds?
+                }
+                MouseScrollUnit::Pixel => {
+                    println!(
+                        "Scroll (pixel units): vertical: {}, horizontal: {}",
+                        ev.y, ev.x
+                    );
+                    projection.scale -= 0.1 * ev.y; //? add delta seconds?
+                }
+            }
+        }
+    }
+}
+
+fn camera_movement(
+    buttons: Res<ButtonInput<MouseButton>>,
+    mut evr_motion: EventReader<MouseMotion>,
+    mut query: Query<&mut Transform, With<Camera>>,
+) {
+    for mut camera_transform in &mut query {
+        if buttons.pressed(MouseButton::Middle) {
+            for ev in evr_motion.read() {
+                println!("Mouse moved: X: {} px, Y: {} px", ev.delta.x, ev.delta.y);
+                camera_transform.translation += Vec3 {
+                    x: ev.delta.x * -1.0,
+                    y: ev.delta.y,
+                    z: 0.0,
+                }
+            }
+        }
+    }
 }
